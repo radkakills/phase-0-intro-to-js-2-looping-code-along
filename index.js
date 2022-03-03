cbad0a9f12ae3018b585b1d61340889e85244bb8
Jeffery Radka <jeffradka9@gmail.com>
2022-03-03 13:39:33 -0500

    Need Help

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const cards = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(cards) {
+  for (let i = 0; i < cards.length; i++) {
+    console.log(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+    debugger;
+  }
+
+  return cards;
+}
+
+writeCards(cards);
+
+let countDown = 10;
+while (countDown > -1) {
+  console.log(countDown--);
+}
+
